fd1ef8f9a0b70a6761f02630f5146f871f8db64b
Pratik <2401pratik@gmail.com>
2025-06-26 14:13:56 +0530

    network module

---
+++ networking/main.tf
@@ -0,0 +1,18 @@
+resource "aws_vpc" "main" {
+    cidr_block = var.vpc_cidr
+    tags = merge(var.tags, {
+        Name = "${var.env}-vpc"
+    })
+}
+
+resource "aws_subnet" "public" {
+    count   = length(var.public_subnets)
+    vpc_id  = aws_vpc.main.id
+    cidr_block = var.public_subnets[counts.index]
+    availability_zone = element(var.azs, count.index)
+    map_public_ip_on_launch = true
+    tags = merge(var.tags, {
+        Name = "${var.env}-public-${count.index + 1}"
+    })
+}
+
---
+++ networking/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_cidr" {}
+variable "public_subnets" { type = list(string) }
+variable "azs" {type = list(string)}
+variable "env" {}
+variable "tags" {type = map(string)}

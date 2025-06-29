6503823a4d3eb0fb47cd1a058049998ebaf91e26
Pratik <2401pratik@gmail.com>
2025-06-29 13:44:29 +0530

    changed variables

--- compute/variables.tf
+++ compute/variables.tf
@@ -3,5 +3,7 @@ variable "ami" {}
 variable "instance_type" {}
 variable "subnet_ids" { type = list(string) }
 variable "key_pair_name" {}
-variable "environment" {}
+variable "environment" {
+  description = "The environment name"
+}
 variable "tags" { type = map(string) }
--- networking/variables.tf
+++ networking/variables.tf
@@ -1,5 +1,7 @@
 variable "vpc_cidr" {}
 variable "public_subnets" { type = list(string) }
 variable "azs" {type = list(string)}
-variable "environment" {}
+variable "environment" {
+  description = "The environment name"
+}
 variable "tags" {type = map(string)}

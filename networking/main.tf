59c5e6fca9e605f03b937322c84afa1db7f34ba6
Pratik <2401pratik@gmail.com>
2025-06-29 13:55:29 +0530

    count (no s)

--- networking/main.tf
+++ networking/main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
     count   = length(var.public_subnets)
     vpc_id  = aws_vpc.main.id
-    cidr_block = var.public_subnets[counts.index]
+    cidr_block = var.public_subnets[count.index]
     availability_zone = element(var.azs, count.index)
     map_public_ip_on_launch = true
     tags = merge(var.tags, {

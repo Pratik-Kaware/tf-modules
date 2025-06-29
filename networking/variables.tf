8603a9377f6a5ec4e4aab47bed8524cf2a461ab1
Pratik <2401pratik@gmail.com>
2025-06-29 13:53:28 +0530

    count (no s)

--- networking/variables.tf
+++ networking/variables.tf
@@ -1,6 +1,8 @@
 variable "vpc_cidr" {}
 variable "public_subnets" { type = list(string) }
-variable "azs" {type = list(string)}
+variable "azs" {
+    type = list(string)
+}
 variable "environment" {
   description = "The environment name"
 }

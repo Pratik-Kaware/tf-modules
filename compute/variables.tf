0b395b7865b5b5af9e466eba36dbd0dee5d8fecc
Pratik-Kaware <83581231+Pratik-Kaware@users.noreply.github.com>
2025-06-30 19:46:51 +0530

    Update variables.tf

--- compute/variables.tf
+++ compute/variables.tf
@@ -2,7 +2,7 @@ variable "instance_count" {}
 variable "ami" {}
 variable "instance_type" {}
 variable "subnet_ids" { type = list(string) }
-variable "key_pair_name" {}
+#variable "key_pair_name" {}
 variable "environment" {
   description = "The environment name"
 }

dbed047a0963662f9b09185e1e28cb93a276ccc6
Pratik-Kaware <83581231+Pratik-Kaware@users.noreply.github.com>
2025-06-30 19:44:39 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "web"{
     ami     = var.ami
     instance_type   = var.instance_type
     subnet_id   = var.subnet_ids[count.index % length(var.subnet_ids)]
-    key_name    = var.key_pair_name
     tags    = merge(var.tags, {
         Name = "${var.environment}-web-${count.index + 1 }"
     })

0c2c6641a1980fa44c9c7b4cdda00cdc3b3a83a2
Pratik <2401pratik@gmail.com>
2025-06-30 20:00:06 +0530

    removed key pait

--- compute/variables.tf
+++ compute/variables.tf
@@ -2,7 +2,6 @@ variable "instance_count" {}
 variable "ami" {}
 variable "instance_type" {}
 variable "subnet_ids" { type = list(string) }
-#variable "key_pair_name" {}
 variable "environment" {
   description = "The environment name"
 }
